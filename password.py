ac9cf2b4b1a68a4e688785ff6ff0873d3ed4f06c
yuta136 <yutak136@outlook.jp>
2023-05-27 17:45:34 +0900

    password

--- password.py
+++ password.py
@@ -1,3 +1,12 @@
 print("パスワード")
-word = input("文字")
+word = input("mozi")
 print(word)
+
+import random
+
+number= input("文字数：")
+decide_number = int(number)
+for i in range(decide_number):
+    random_number = random.randint(1, 9)
+    print(random_number)
+
