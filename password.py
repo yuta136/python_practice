558f2ee852f0a74ec1fa7c27ee72b19d6e9e5ba5
yuta136 <yutak136@outlook.jp>
2023-05-27 18:28:19 +0900

    +word

--- password.py
+++ password.py
@@ -3,10 +3,14 @@ word = input("mozi")
 print(word)
 
 import random
+import string
 
+random_number=list(range(1, 10))*2
+random_word= list (string.ascii_lowercase)
+choices=random_number + random_word
 number= input("文字数：")
 decide_number = int(number)
 for i in range(decide_number):
-    random_number = random.randint(1, 9)
-    print(random_number)
+    random_pass = random.choice(choices)
+    print(random_pass)
 
